24c037545c9cedf9e3cd7b6e486ffe166ad2c4c8
akiselef <rage4ndlove@gmail.com>
2021-02-10 17:58:30 +0400

    pure selenide solution add solution with lambdas add

---
+++ src/test/java/PureSelenideSearchTest.java
@@ -0,0 +1,38 @@
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PureSelenideSearchTest {
+
+    private static final String BASE_URL = "https://github.com/";
+    private static final String REPO = "akiselef/basicAllureDemo";
+    private static final String ISSUE_NUMBER = "#2";
+
+    @Test
+    @Owner("akiselef")
+    @Severity(SeverityLevel.CRITICAL)
+    @Tags({@Tag("demo"), @Tag("web")})
+    @Link(name = "Github Main Page", value = BASE_URL)
+    @Feature("Issues check")
+    @Story("ZG-1337")
+    @DisplayName("Search for issue by it's number (pure Selenide)")
+    void searchForIssueTest() {
+        open(BASE_URL);
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(REPO);
+        $(".header-search-input").submit();
+        $(By.linkText(REPO)).click();
+        $(withText("Issues")).click();
+        $(withText(ISSUE_NUMBER)).should(exist);
+
+    }
+}
---
+++ src/test/java/SearchTestWithLambdaSteps.java
@@ -0,0 +1,84 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class SearchTestWithLambdaSteps {
+
+    private static final String BASE_URL = "https://github.com/";
+    private static final String REPO = "akiselef/basicAllureDemo";
+    private static final String ISSUE_NUMBER = "#2";
+    private static final String ISSUES = "Issues";
+
+
+    @Test
+    @Owner("akiselef")
+    @Severity(SeverityLevel.CRITICAL)
+    @Tags({@Tag("demo"), @Tag("web")})
+    @Link(name = "Github Main Page", value = BASE_URL)
+    @Feature("Issues check")
+    @Story("ZG-1337")
+    @DisplayName("Search for issue by it's number (/w lambdas)")
+    void lambdasSearchForIssueTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Opening github main page", () -> {
+            open(BASE_URL);
+        });
+        step("Searching for " + REPO, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPO);
+            $(".header-search-input").submit();
+        });
+        step("Clicking on " + REPO + " link", () -> {
+            $(By.linkText(REPO)).click();
+        });
+        step("Selecting " + ISSUES + " tab", () -> {
+            $(withText("Issues")).click();
+        });
+        step("Checking that issue with issue number " + ISSUE_NUMBER + " exists", () -> {
+            $(withText(ISSUE_NUMBER)).should(exist);
+        });
+    }
+
+    @Test
+    @Owner("akiselef")
+    @Severity(SeverityLevel.CRITICAL)
+    @Tags({@Tag("demo"), @Tag("web"), @Tag("Failed_on_purpose")})
+    @Link(name = "Github Main Page", value = BASE_URL)
+    @Feature("Issues check")
+    @Story("ZG-1338")
+    @DisplayName("Failed test (I did it for the science!)")
+    void lambdasFailedTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Opening github main page", () -> {
+            open(BASE_URL);
+        });
+        step("Searching for " + REPO, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPO);
+            $(".header-search-input").submit();
+        });
+        step("Clicking on " + REPO + " link", () -> {
+            $(By.linkText(REPO)).click();
+        });
+        step("Selecting " + ISSUES + " tab", () -> {
+            $(withText("Issues123")).click();
+        });
+        step("Checking that issue with issue number " + ISSUE_NUMBER + " exists", () -> {
+            $(withText(ISSUE_NUMBER)).should(exist);
+        });
+    }
+
+}
